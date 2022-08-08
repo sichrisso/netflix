283ca90562bfeb611a537e408be8ebfd45faf438
sichrisso <helenasolomon87@gmail.com>
2022-08-08 23:36:19 +0300

    add popup

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const navbar = document.querySelector('.navbar');
 const play = document.getElementById('playbtn');
-const detail = document.querySelector('.detailPage');
+const detail = document.querySelector('.body2');
 const movie =document.querySelector('.good');
 const close = document.querySelector('.close');
+const maindiv = document.querySelector('.body1');
 
 window.onscroll = function () {
     let top = window.scrollY;
@@ -15,9 +16,11 @@ window.onscroll = function () {
 
 
 movie.addEventListener("click", function() {
-    detail.classList.add('active')
+    detail.classList.add('active');
+    maindiv.classList.add('active');
 })
 
 close.addEventListener("click", function() {
-    detail.classList.remove('active')
+    detail.classList.remove('active');
+    maindiv.classList.remove('active');
 })

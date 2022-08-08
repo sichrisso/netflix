569c2b71fb285ff51d13324a18a38c2fec69c15e
sichrisso <helenasolomon87@gmail.com>
2022-08-08 18:06:06 +0300

    fix size

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 const navbar = document.querySelector('.navbar');
+const play = document.getElementById('playbtn');
+const ju = document.querySelector('#C13');
+const detail = document.querySelector('.detailPage');
+const movie =document.querySelector('.good');
 
 window.onscroll = function () {
     let top = window.scrollY;
@@ -8,3 +12,13 @@ window.onscroll = function () {
         navbar = navbar.classList.remove('active')
     }
 }
+
+
+ju.addEventListener("onmouseenter", function() {
+    ju.innerHTML
+    play.style.display = "block";
+})
+
+movie.addEventListener("click", function() {
+    detail.classList.add('active')
+})

46333c0ba43d1cc67329ee5b05f7a9ceb0aab892
sichrisso <helenasolomon87@gmail.com>
2022-08-17 17:00:13 +0300

    final touch

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const detail = document.querySelector(".body2");
 const movie = document.querySelector(".good");
 const closing = document.querySelector(".cancel");
 const maindiv = document.querySelector(".body1");
-const propagate = document.querySelector(".detailPage");
+const detailPage = document.querySelector(".detailPage");
 const nxtButtons = [...document.getElementsByClassName("nxtBtn")];
 const preButtons = [...document.getElementsByClassName("preBtn")];
 const MovieScrolls = [...document.getElementsByClassName("ImageContainer")];
@@ -11,8 +11,10 @@ const Searchbar = document.querySelector(".search");
 const Setting = document.querySelector(".setting");
 const SearchText = document.querySelector(".searchText");
 const SearchField = document.querySelector(".searchField");
-const detailBackground = document.querySelector("#detail_img");
+const detailOverview = document.querySelector("#DetailOverview");
 const detailInformation = document.querySelector(".details");
+const detailTopContent = document.querySelector(".top_content")
+
 
 // Top NavBar on scroll background color
 window.onscroll = function () {
@@ -27,24 +29,25 @@ window.onscroll = function () {
 //  Pass data to detail page
 function movieDetail(title, overview, image) {
   let DetailInfo = "";
-  DetailInfo = `
-  <div id="detail_img"  style="background-image: linear-gradient(
-    to bottom,
-    transparent 0%,
-    rgb(34, 34, 34) 100%
-  ),
-  url('https://image.tmdb.org/t/p/original/${image}')"></div>
+  DetailInfo = `<div class="details">
   <img class="cancel" src="images/close.svg" alt="exit" onclick= "cancel()" />
   <h1 id="detail_title">${title}</h1>
   <p>${overview}</p>
-  `;
-    
+</div>`;
+detailOverview.textContent = `${overview}`;
+detailTopContent.style.backgroundImage = `linear-gradient(
+    to bottom,
+    transparent 0%,
+    rgb(34, 34, 34) 100%
+  ),url('https://image.tmdb.org/t/p/original/${image}')`;
+  document.querySelector("#dynamicCollection").textContent = `${title} Collections`
+  document.querySelector("#aboutMovie").textContent = `About ${title}`
   detailInformation.innerHTML = DetailInfo;
   detail.classList.add("active");
   maindiv.classList.add("active");
 }
 
-// Detail page closing 
+// Detail page closing
 function cancel() {
   detail.classList.remove("active");
   maindiv.classList.remove("active");
@@ -55,14 +58,13 @@ detail.addEventListener("click", function () {
   maindiv.classList.remove("active");
 });
 
-propagate.addEventListener("click", function (e) {
+detailPage.addEventListener("click", function (e) {
   e.stopPropagation();
 });
 
-
 /* Search Bar */
 Searchbar.addEventListener("click", function () {
-  console.log("clicked")
+  console.log("clicked");
   SearchField.focus();
   Searchbar.classList.add("active");
   SearchText.classList.add("active");
